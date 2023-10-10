e0b5f095009d656b92002b591bde27ba640e6289
gauravshrestha <imgauravshrestha@gmail.com>
2023-10-10 13:39:48 +0545

    moving file

--- app.js
+++ app.js
@@ -10,18 +10,24 @@ const cookieParser = require("cookie-parser"); // Added for cookie handling
 // Define a global variable to store CSV data
 let csvData = [];
 
-// Read CSV data when the server starts
-fs.createReadStream(path.resolve(__dirname, "nameList.csv"))
-  .pipe(csv({ headers: ["RollNumber", "Name"] })) // Specify column headers
-  .on("data", (row) => {
-    const rollNumber = row["RollNumber"];
-    const name = row["Name"];
-    csvData.push({ rollNumber, name });
-  })
-  .on("end", () => {
-    console.log("CSV data loaded successfully");
-  });
+// Function to load CSV data
+function loadCsvData() {
+  fs.createReadStream(path.resolve(__dirname, "nameList.csv"))
+    .pipe(csv({ headers: ["RollNumber", "Name"] })) // Specify column headers
+    .on("data", (row) => {
+      const rollNumber = row["RollNumber"];
+      const name = row["Name"];
+      csvData.push({ rollNumber, name });
+    })
+    .on("end", () => {
+      console.log("CSV data loaded successfully");
+    });
+}
 
+// Load CSV data when the server starts
+loadCsvData();
+
+// Configure Express
 app.set("view engine", "ejs");
 app.set("views", "views");
 
@@ -29,13 +35,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); // Use cookie parser middleware
 
+// Define routes
+
+// Homepage
 app.get("/", (req, res) => {
-  res.render("index", { data: csvData });
+  const selectedName = req.cookies.selectedName || ""; // Get the selected name from cookies
+  const labNumber = req.cookies.labNumber || ""; // Get the lab number from cookies
+  res.render("index", { data: csvData, selectedName, labNumber });
 });
 
 app.get("/success/:docxFileName", (req, res) => {
   const docxFileName = req.params.docxFileName;
-  res.render("success", { docxFileName });
+  res.render("success", {
+    docxFileName,
+    name: req.cookies.name || "", // Read "name" cookie
+    labnumber: req.cookies.labnumber || "", // Read "labnumber" cookie
+  });
 });
 
 app.get("/download/:docxFileName", (req, res) => {
@@ -43,8 +58,14 @@ app.get("/download/:docxFileName", (req, res) => {
   const docxFilePath = path.resolve(__dirname, "temp", docxFileName);
 
   // Send the generated DOCX file as a response
-  res.setHeader("Content-Disposition", `attachment; filename="${docxFileName}"`);
-  res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+  res.setHeader(
+    "Content-Disposition",
+    `attachment; filename="${docxFileName}"`
+  );
+  res.setHeader(
+    "Content-Type",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+  );
   const fileStream = fs.createReadStream(docxFilePath);
   fileStream.pipe(res);
 
@@ -52,22 +73,29 @@ app.get("/download/:docxFileName", (req, res) => {
   res.cookie("downloadComplete", "true");
 
   // After the download is complete, the user will be redirected to the index page
+  res.cookie("downloadComplete", "true", { expires: new Date(0) }); // Clear the downloadComplete cookie
 });
 
+// Create DOCX File
 app.post("/createDocx", async (req, res) => {
   const { name, labnumber } = req.body;
 
-  const content = fs.readFileSync(
-    path.resolve(__dirname, "template.docx"),
-    "binary"
-  );
+  // Set cookies to store the selected values
+  res.cookie("selectedName", name);
+  res.cookie("labNumber", labnumber);
 
+
+  // Load the DOCX template content
+  const content = fs.readFileSync(path.resolve(__dirname, "template.docx"), "binary");
   const zip = new PizZip(content);
 
+  // Initialize the Docxtemplater
   const doc = new Docxtemplater(zip, {
     paragraphLoop: true,
     linebreaks: true,
   });
+
+  // Helper functions
 
   function getRollNumber(selectedName) {
     const selectedData = csvData.find((item) => item.name === selectedName);
@@ -97,6 +125,8 @@ app.post("/createDocx", async (req, res) => {
     }
   }
 
+  // Render the DOCX document
+
   const rollno = getRollNumber(name);
   const section = getSection(rollno);
 
@@ -107,15 +137,16 @@ app.post("/createDocx", async (req, res) => {
     section: section,
   });
 
+  // Generate the DOCX buffer
+
   const buf = doc.getZip().generate({
     type: "nodebuffer",
     compression: "DEFLATE",
   });
 
+  // Generate a unique timestamp for the file name
   const timestamp = Date.now();
-  const docxFileName = `${getFirstName(
-    req.body.name
-  ).toLowerCase()}_lab_${labnumber}_${timestamp}.docx`;
+  const docxFileName = `${getFirstName(name).toLowerCase()}_lab_${labnumber}_${timestamp}.docx`;
 
   // Save the generated DOCX file to a temporary folder
   const tempFolderPath = path.resolve(__dirname, "temp");
@@ -127,6 +158,10 @@ app.post("/createDocx", async (req, res) => {
   res.redirect(`/success/${docxFileName}`);
 });
 
-app.listen(3000, () => {
-  console.log("Node.js project started at port 3000");
+// Start the server
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Node.js project started at port ${PORT}`);
 });
